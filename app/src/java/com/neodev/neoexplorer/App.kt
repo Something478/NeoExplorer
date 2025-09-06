9cb01cfd44df5276305f2bb1858930d357756183
ChillbyteHD <m66361544@gmail.com>
2025-09-06 07:53:59 +0300

    update app/src/java/com/neodev/neoexplorer/App.kt

---
+++ app/src/java/com/neodev/neoexplorer/App.kt
@@ -0,0 +1,11 @@
+package com.neodev.neoexplorer
+
+import android.app.Application
+import androidx.multidex.MultiDex
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        MultiDex.install(this)
+    }
+}
